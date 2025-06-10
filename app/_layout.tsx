84d44f2b7c902d628d6d3815f4ca75e11d6eb19f
Đỗ Quang Dũng <111267984+DoDung-BePresent@users.noreply.github.com>
2025-06-10 10:11:57 +0700

    update mainlayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,44 +1,48 @@
 import { Color } from "@/utils/Color";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import { Tabs } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Color.accent,
-        tabBarInactiveTintColor: Color.placeholder,
-        tabBarStyle: {
-          backgroundColor: Color.background,
-          borderTopColor: Color.accent,
-        },
-        headerStyle: {
-          backgroundColor: Color.background,
-        },
-        headerTitleStyle: {
-          color: Color.text,
-        },
-        headerTitleAlign: "center",
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Generate",
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome5 name="magic" size={size} color={color} />
-          ),
+    <>
+      <StatusBar backgroundColor={Color.background} translucent={false} />
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Color.accent,
+          tabBarInactiveTintColor: Color.placeholder,
+          tabBarStyle: {
+            backgroundColor: Color.background,
+            borderTopColor: Color.accent,
+          },
+          headerStyle: {
+            backgroundColor: Color.background,
+          },
+          headerTitleStyle: {
+            color: Color.text,
+          },
+          headerTitleAlign: "center",
         }}
-      />
-      <Tabs.Screen
-        name="history"
-        options={{
-          title: "History",
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome5 name="history" size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Generate",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="magic" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="history"
+          options={{
+            title: "History",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="history" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
